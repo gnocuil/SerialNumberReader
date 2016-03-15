0ce14f778f8244ae2ad9c35e6754bc75bbfc1380
gnocuil <gnocuil@gmail.com>
2016-03-15 14:55:44 +0800

    support basic ocr; need more test

--- image.py
+++ image.py
@@ -2,10 +2,12 @@ import Image
 import codecs
 import Queue
 import string,os,sys
+from pytesser import *
 
 threshold = 160
 dx=[0,1,0,-1]
 dy=[1,0,-1,0]
+exe='C:\\Program Files (x86)\\Tesseract-OCR\\tesseract.exe'
 
 def roll(image, delta):
     xsize, ysize = image.size
@@ -20,11 +22,24 @@ def roll(image, delta):
 
     return image
 
+def ocr(filename):
+    print exe
+    args = [exe, filename, 'out','-psm 7']
+    proc = subprocess.Popen(args)
+    retcode = proc.wait()
+    if retcode!=0:
+        return ''
+    f=open("out.txt", "r")
+    return f.readline()
+    
+    
 def readImage(filename):
     im = Image.open(filename)
     prefix=os.path.splitext(filename)[0]
     print prefix
     im=im.rotate(270)#TODO
+    w,h=im.size
+    im=im.resize((w/2, h/2),Image.ANTIALIAS)
     w,h=im.size
     region = (w/6,h/2,w-1,h*3/4)
     im=im.crop(region)
@@ -88,25 +103,43 @@ def readImage(filename):
     f.close()
     bim = Lim.point(table, '1')
     if best>0:
-        region = (w1,h1,w2,h2)
+        w=w2-w1
+        h=h2-h1
+        region = (w1+w/8,h1+h/6,w2-w/8,h2-h/6)
         tim=bim.crop(region)
-        tim.show()
+        pix=tim.load()
+        w,h=tim.size
+        for i in range(h/4):
+            for j in range(w/10):
+                pix[j,i]=1
+        
+        #tim.show()
         tim.save('final.jpg')
+        #print image_to_string(tim)
+        ret=ocr('final.jpg')
+        print ret
+        return ret
     else:
         #failed to find'
         print 'failed...'
     im.save('original.jpg')
+    return ''
     
 def readDir(dir,count=-1):
     files = os.listdir(dir)  
+    fout=codecs.open(dir+os.sep+'serial.csv', "w", 'utf-8')
+    id=1
     for f in files:
         if count==0: break
         path=dir+os.sep+f
         suf=os.path.splitext(path)[1].lower()
         if suf=='.jpg' or suf=='.png':
-            readImage(path)
+            ret=readImage(path)
+            ret=ret.replace(' ',',').strip('\n')
+            print >>fout,`id`+','+ret+','+f
             count=count-1
+            id=id+1
     
 #main
-#readImage('votes/IMG_6176.JPG')
+#readImage('votes/IMG_6175.JPG')
 readDir('votes')
